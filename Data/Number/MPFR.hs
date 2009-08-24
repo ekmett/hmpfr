91580bf7e30a55975d256069b569c86cc345bfbf
Ales Bizjak <ales.bizjak0@gmail.com>
2009-08-24 06:33:43 -0700

    Cleanup in Data.Number.MPFR.hs.
    Ignore-this: a7fc6fc0f00d3ff46c1c441afee9cfa3
    
    darcs-hash:20090824133343-5fe1c-fcafd34aa1026052aa5fe27aca4329215cba605b.gz

--- Data/Number/MPFR.hs
+++ Data/Number/MPFR.hs
@@ -107,15 +107,6 @@ addPrec d1 d2 = fromIntegral (max (p1 + e1 - e3) (p2 + e2 - e3)) + 1
                       p2 = fromIntegral $ getPrec d2
                       e3 = min e1 e2
 
-{-
-addPrec d1 d2 = max e1 e2 + 1 - min (e1 - p1) (p2 - e2)
-                where e1 = if d1 == 0 then 0 else fromIntegral $ getExp d1
-                      e2 = if d2 == 0 then 0 else fromIntegral $ getExp d2
-                      p1 = getPrec d1
-                      p2 = getPrec d2
--}
-
-
 instance Real MPFR where
     toRational d = n % 2 ^ e
         where (n', e') = decompose d
